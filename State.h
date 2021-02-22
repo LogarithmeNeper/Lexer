99ac0376e053a9d9aff988cb63b1377cc9970dee
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-02-22 08:56:47 +0100

    first template of states

---
+++ State.h
@@ -0,0 +1,71 @@
+#include <string>
+using namespace std;
+
+class State {
+    public:
+        State();
+        State(string s);
+        ~State();
+    protected:
+
+};
+
+class State0 : public State {
+    public:
+        State0();
+    protected:
+};
+
+class State1 : public State {
+    public:
+        State1();
+    protected:
+};
+
+class State2 : public State {
+    public:
+        State2();
+    protected:
+};
+
+class State3 : public State {
+    public:
+        State3();
+    protected:
+};
+
+class State4 : public State {
+    public:
+        State4();
+    protected:
+};
+
+class State5 : public State {
+    public:
+        State5();
+    protected:
+};
+
+class State6 : public State {
+    public:
+        State6();
+    protected:
+};
+
+class State7 : public State {
+    public:
+        State7();
+    protected:
+};
+
+class State8 : public State {
+    public:
+        State8();
+    protected:
+};
+
+class State9 : public State {
+    public:
+        State9();
+    protected:
+};
