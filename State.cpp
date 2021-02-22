4e5256f09fa1aefaab8715a0c314e296c963ddf0
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-02-22 09:43:57 +0100

    names

--- State.cpp
+++ State.cpp
@@ -1,2 +1,41 @@
 #include "State.h"
 
+State0::State0() {
+    name = "I0";
+}
+
+State1::State1() {
+    name = "I1";
+}
+
+State2::State2() {
+    name = "I2";
+}
+
+State3::State3() {
+    name = "I3";
+}
+
+State4::State4() {
+    name = "I4";
+}
+
+State5::State5() {
+    name = "I5";
+}
+
+State6::State6() {
+    name = "I6";
+}
+
+State7::State7() {
+    name = "I7";
+}
+
+State8::State8() {
+    name = "I8";
+}
+
+State9::State9() {
+    name = "I9";
+}
--- State.h
+++ State.h
@@ -7,6 +7,7 @@ class State {
         State(string s);
         ~State();
     protected:
+        string name;
 
 };
 
