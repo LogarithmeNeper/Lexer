4ad7b6fa54c15acae5b294019b272c20ca3a8d27
Thibaut Gillain <thibaut.gillain@insa-lyon.fr>
2021-03-03 13:11:42 +0100

    add a bool to stop main loop

--- main.cpp
+++ main.cpp
@@ -4,19 +4,25 @@
 
 
 int main(void) {
-   while(true) {
+   bool continueLoop = true;
+   while(continueLoop) {
       string chaine;
-      std::cout << "Insérer la chaîne à analyser" << endl;
+      std::cout << "Insérer la chaîne à analyser (STOP pour arrêter le programme)" << endl;
       std::cin >> chaine;
-      Automaton *a = new Automaton(chaine);
-      int res=a->run();
-      if(res!=-1) {
-         cout << "Expression reconnue" << endl;
-         cout << "Résultat : " << res << endl;
+      if(chaine == "STOP") {
+         continueLoop = false;
+      } else {
+         Automaton *a = new Automaton(chaine);
+         int res=a->run();
+         if(res!=-1) {
+            cout << "Expression reconnue" << endl;
+            cout << "Résultat : " << res << endl;
+         }
+         else {
+            cout << "Expression non reconnue" << endl;
+         }
       }
-      else {
-         cout << "Expression non reconnue" << endl;
-      }
+      
    }
    return 0;
 }
